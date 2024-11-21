60befd30f38b587758e38e7bb4c5cf4b0f462c96
Jessiessalgado <jessica.silvasalgado@icloud.com>
2024-11-21 15:35:19 -0300

    Add. logica de duelo, efeito sonoro

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -13,6 +13,12 @@ const state = {
         player: document.getElementById("player-infield-card"),
         computer: document.getElementById("computer-infield-card"),
     },
+    playerSides: {
+        player1: "player-cards",
+        player1BOX: document.querySelector("#player-cards"), 
+        computer: "computer-cards",
+        computerBOX: document.querySelector("#computer-cards"),
+    },
     actions: {
         button: document.getElementById("next-duel"),
     },
@@ -94,6 +100,47 @@ async function setCardsField(cardId) {
     await drawButton(duelResults);
 }
 
+async function drawButton(text) {
+    state.actions.button.innerText = text.toUpperCase();
+    state.actions.button.style.display = "block";
+}
+
+async function updateScore() {
+    state.score,scoreBox.innerText = `Win: ${state.score.playerScore} | Lose: ${state.score.computerScore}`
+
+
+
+}
+
+/* Lógica do duelo */
+async function checkDuelResults(playerCardId, computerCardId) {
+    let duelResults = "Deaw";
+    let playerCard = cardData[playerCardId];
+
+    if (playerCard.WinOf.includes(computerCardId)){
+        duelResults = "win"
+        state.score.playerScore++;
+    }
+    if (playerCard.LoseOf.includes(computerCardId)){
+        duelResults = "lose"
+        state.score.computerScore++;
+    }
+
+    await playAudio(duelResults);
+
+    return duelResults;
+}
+
+/* Implementando o removeAllCardImages */
+async function removeAllCardImage() {
+    let {computerBOX, player1BOX} = state.playerSides;
+    let imgElements = computerBOX.querySelectorAll("img");
+    imgElements.forEach((img) => img.remove());
+
+    imgElements = player1BOX.querySelectorAll("img");
+    imgElements.forEach((img) => img.remove());
+}
+
 async function drawSelectCards(index) {
     state.cardSprites.avatar.src = cardData[index].img;
     state.cardSprites.name.innerText = cardData[index].name;
@@ -107,6 +154,24 @@ async function drawCards(cardNumbers, fieldSide) {
         const cardImage = await createCardImage(randomIdCard, fieldSide);
         document.getElementById(fieldSide).appendChild(cardImage);
     }
+}
+
+async function resetDuel() {
+    state.cardSprites.avatar.src = "";
+    state.actions.button.style.display = "none";
+
+    state.fieldCards.player.style.display = "none"
+    state.fieldCards.computer.style.display = "none"
+
+    init();
+}
+/* Implementando audio */
+async function playAudio(status) {
+    const audio = new Audio(`./src/assets/audios/${status}.wav`);
+    try {
+        audio.play();
+    } catch {}
+    
 }
 
 function init() {
